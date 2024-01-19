3d5fc911de57079dc77ad4ead03dd4bd10592096
Caleb Leinz <caleb.leinz@atlasground.com>
2024-01-18 16:36:21 -0800

    Make DelimiterSlice generic over AsRef<[u8]>

--- src/lib.rs
+++ src/lib.rs
@@ -28,25 +28,25 @@ use pin_project_lite::pin_project;
 pin_project! {
     #[derive(Debug)]
     #[must_use = "streams do nothing unless polled"]
-    pub struct DelimiterSlice<'a, St> {
+    pub struct DelimiterSlice<St, D> {
         #[pin]
         stream: St,
     buf: BytesMut,
-        delimiter: &'a [u8],
+        delimiter: D,
     found: bool,
     }
 }
 
-impl<'a, St> DelimiterSlice<'a, St> {
+impl<St, D> DelimiterSlice<St, D> {
     /// Create a new `DelimiterSlice` based on the provided stream and delimiter.
     ///
     /// This defaults instantiating the underlying buffer with a capacity of 8,192 bytes.
-    pub fn new(stream: St, delimiter: &'a [u8]) -> Self {
-	Self::with_capacity(stream, 8_192, delimiter)
+    pub fn new(stream: St, delimiter: D) -> Self {
+        Self::with_capacity(stream, 8_192, delimiter)
     }
 
     /// Create a new `DelimiterSlice` based on the provided stream, delimiter, and capacity.
-    pub fn with_capacity(stream: St, capacity: usize, delimiter: &'a [u8]) -> Self {
+    pub fn with_capacity(stream: St, capacity: usize, delimiter: D) -> Self {
         Self {
             stream,
             buf: BytesMut::with_capacity(capacity),
@@ -64,17 +64,18 @@ impl<'a, St> DelimiterSlice<'a, St> {
     ///
     /// Panics if the internal buffer is not empty when this is called. The stated purpose of this
     /// library is to provide a simple and safe way to extract data from a delimited stream, but
-    /// allow the stream to continue producing the data in the order it was received. 
+    /// allow the stream to continue producing the data in the order it was received.
     ///
     /// If you've called into_inner before a second call to `next` this is likely an error.
     pub fn into_inner(self) -> St {
-	assert!(self.buf.is_empty());
+        assert!(self.buf.is_empty());
         self.stream
     }
 }
 
-impl<'a, St> Stream for DelimiterSlice<'a, St>
+impl<St, D> Stream for DelimiterSlice<St, D>
 where
+    D: AsRef<[u8]>,
     St: Stream<Item = Result<Bytes, std::io::Error>>,
 {
     type Item = Result<Bytes, std::io::Error>;
@@ -82,25 +83,26 @@ where
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<St::Item>> {
         let mut this = self.project();
 
-	if *this.found {
-	    if this.buf.is_empty() {
-		return this.stream.poll_next(cx);
-	    } else {
-		return Poll::Ready(Some(Ok(this.buf.split().freeze())));
-	    }
-	}
+        if *this.found {
+            if this.buf.is_empty() {
+                return this.stream.poll_next(cx);
+            } else {
+                return Poll::Ready(Some(Ok(this.buf.split().freeze())));
+            }
+        }
 
         let mut exhausted = false;
 
         loop {
+            let delim = this.delimiter.as_ref();
             if let Some(index) = this
                 .buf
-                .windows(this.delimiter.len())
-                .position(|window| window.eq(this.delimiter.as_ref()))
+                .windows(delim.len())
+                .position(|window| window.eq(delim))
             {
                 let data = this.buf.split_to(index).freeze();
-                this.buf.advance(this.delimiter.len());
-		*this.found = true;
+                this.buf.advance(delim.len());
+                *this.found = true;
                 return Poll::Ready(Some(Ok(data)));
             }
 
@@ -114,26 +116,5 @@ where
                 None => exhausted = true,
             }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    use futures_util::StreamExt;
-    use tokio_util::io::ReaderStream;
-
-    #[tokio::test]
-    async fn it_works() {
-	const TEST: &[u8] = b"FOOBARFOOBARBAZFOO";
-	const DELIM: &[u8] = b"BAZ";
-
-	let stream = ReaderStream::new(TEST);
-	let mut slice_stream = DelimiterSlice::new(stream, DELIM);
-	let data = slice_stream.next().await.unwrap().unwrap();
-	assert_eq!(&data, &TEST[0..12]);
-	let data = slice_stream.next().await.unwrap().unwrap();
-	assert_eq!(&data, &TEST[15..]);
     }
 }
